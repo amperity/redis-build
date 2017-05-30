1b827c3597d9e980258d301ce13681d4da98deba
Chuck Sakoda <chuck@amperity.com>
2017-05-30 14:07:12 -0700

    Build locally

--- stage/build.sh
+++ stage/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fpm -f -s dir -t deb -n redis-server -v 4.0-rc3 -C /opt/debs/stage/redis-4.0-rc3/ -p redis-server-4.0-rc3_amd64.deb usr/bin etc
+fpm -f -s dir -t deb -n redis-server -v 4.0-rc3 -C ./redis-4.0-rc3/ -p redis-server-4.0-rc3_amd64.deb usr/bin etc
